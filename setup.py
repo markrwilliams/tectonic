693392abaa3fa5603010e0310f325fc9f05aa6c0
Mark Williams <markrwilliams@gmail.com>
2014-02-09 22:53:01 -0800

    license + setup cleanup

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+__author__ = 'Mark Williams'
+__version__ = '0.0'
+__contact__ = 'markrwilliams@gmail.com'
+__url__ = 'https://github.com/markrwilliams/tectonic'
+__license__ = 'BSD'
+
+
 
 with open('requirements.txt') as f:
     requirements = f.read()
 
 
 setup(name='tectonic',
-      version='0.0.0.0.0.1',
+      author=__author__,
+      author_email=__contact__,
+      version=__version__,
+      license=__license__,
+      url=__url__,
       install_requires=requirements,
       packages=find_packages())
